93d73bfe9dceb146f0f7d979443cb2f3e72fea96
grady-h <116502434+grady-h@users.noreply.github.com>
2023-04-15 22:52:01 -0400

    Finish total message count and baseline features

--- noac_bot.py
+++ noac_bot.py
@@ -1,20 +1,10 @@
-'''
-IMPLEMENTATION
-
-Daily History:
-- Keep a daily tally of how many messages each person has sent in the server
-- If a person goes over a certain daily message threshold, assign Needs To Touch Grass role for a day
-
-- Every night at 12:00am:
-    - Reset the message counts
-'''
-
 import discord, os
 from discord.ext import commands
 from dotenv import load_dotenv
 from touch_grass_cog import TouchGrass
 
 load_dotenv()
+# TODO: review intents
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='/', intents=intents)
 
@@ -27,26 +17,47 @@ async def on_ready():
     print(f"{bot.user} has connected to the server!")
     server = bot.get_guild(SERVER_ID)
     general_channel = bot.get_channel(GENERAL_CHANNEL_ID)
+    member_msg_count = {}
+
+    # get all channels
+    channels = []
+    for channel in bot.get_all_channels():
+        # exclude non text channels
+        if type(channel) != discord.channel.TextChannel:
+            continue
+        channels.append(channel)
+
+    # count the number of messages each member has sent
+    for channel in channels:
+        async for message in channel.history(limit=None):
+            if message.author.bot:
+                continue
+            try:
+                member_msg_count[message.author] += 1
+            except KeyError:
+                member_msg_count[message.author] = 1
     
+    # find member with max message count
+    touch_grass_member = list(member_msg_count.keys())[0]
+    for member in member_msg_count:
+        if member_msg_count[member] > member_msg_count[touch_grass_member]:
+            touch_grass_member = member
+
     # create role
-    await server.create_role(name="Needs To Touch Grass", color=discord.Colour.dark_green())
+    # await server.create_role(name="Needs To Touch Grass", color=discord.Colour.dark_green())
     touch_grass_role = discord.utils.get(server.roles, name="Needs To Touch Grass")
 
+    # TODO: send initial message
+    # assign member with role
+    await touch_grass_member.add_roles(touch_grass_role)
+
+    for member in member_msg_count:
+        member_msg_count[member] = 0
+
     # add touch grass cog
-    await bot.add_cog(TouchGrass(bot, SERVER_ID, general_channel, touch_grass_role))
+    await bot.add_cog(TouchGrass(bot, general_channel, touch_grass_role, touch_grass_member, member_msg_count))
 
     print("Finish initializing")
-
-
-# commands
-@bot.command()
-async def ping(ctx):
-    await ctx.channel.send("pong")
-
-@bot.command()
-async def send(ctx, num):
-    for i in range(int(num)):
-        await ctx.send(f"Placeholder message #{i+1}")
 
 
 bot.run(os.getenv('TOKEN'))
--- touch_grass_cog.py
+++ touch_grass_cog.py
@@ -1,21 +1,20 @@
 import datetime
 from discord.ext import commands, tasks
 from zoneinfo import ZoneInfo
-from member_cog import TouchGrassMember
-
 
 EST = ZoneInfo('US/Eastern')
 daily_reset_time = datetime.time(hour=0, minute=0, tzinfo=EST)
 
-
 class TouchGrass(commands.Cog):
-    def __init__(self, bot, server_id, general, role):
-        self.DAILY_THRESHOLD = 50
-        self.SERVER_ID = server_id
+    def __init__(self, bot, general, role, member, msg_counts):
+        self.DAILY_THRESHOLD = 5
         self.bot = bot
         self.general_channel = general
-        self.daily_msg_count = {}
         self.touch_grass_role = role
+        self.touch_grass_member = member
+        self.member_msg_count = msg_counts
+        self.daily_msg_count = {}
+        self.must_go_outside = []
 
         self.init_msg_counts()
         self.reset_daily_counts.start()
@@ -28,36 +27,68 @@ class TouchGrass(commands.Cog):
             return
         print(f"Increasing {member}'s message count")
         # increase member's message count
+        self.member_msg_count[member] += 1
         self.daily_msg_count[member] += 1
+
+        # DAILY THRESHOLD CHECK
+        # check if member is over daily threshold and hasn't already been called out
+        if self.daily_msg_count[member] > self.DAILY_THRESHOLD and not member in self.must_go_outside:
+            print(f"{member} is over the daily threshold!")
+            # send message
+            await self.general_channel.send(f"<@{member.id}> go outside bruh")
+            # add to must go outside list
+            self.must_go_outside.append(member)
+
+        # TOUCH GRASS ROLE CHECK
         # check if member already has role
         if member.get_role(self.touch_grass_role.id) != None:
             return
-        # check if member is over daily threshold
-        if self.daily_msg_count[member] > self.DAILY_THRESHOLD:
-            print(f"{member} is over the daily threshold!")
-            # give member role
+        # check if member has more messages than current touch grass member
+        member_count = self.member_msg_count[member]
+        current_max = self.member_msg_count[self.touch_grass_member]
+        if member_count > current_max:
+            # remove role from current max member
+            await self.touch_grass_member.remove_roles(self.touch_grass_role)
+            # make author new touch grass member
             await member.add_roles(self.touch_grass_role)
-            # add a new cog for the member
-            await self.bot.add_cog(TouchGrassMember(self.bot, self.SERVER_ID, member, self.touch_grass_role))
+            self.touch_grass_member = member
             # send message
-            await self.general_channel.send(f"<@{member.id}> go outside bruh")
+            await self.general_channel.send(f"<@{self.touch_grass_member.id}> is the new non-grass toucher with {member_count} messages. PLEASE go outside dawg")
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        print("Adding new member to message count dictionary")
-        # add a new entry in the message count dictionary
+        print("Adding new member to message count dictionaries")
+        # add a new entry in the message count dictionaries
+        self.member_msg_count[member] = 0
         self.daily_msg_count[member] = 0
-    
-    # background tasks
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        print("Removing old member from message count dictionaries")
+        del self.member_msg_count[self.touch_grass_member]
+        del self.daily_msg_count[self.touch_grass_member]
+        if member == self.touch_grass_member:
+            print("Transferring Touch Grass role to second place member")
+            # find member with next highest message count
+            self.touch_grass_member = list(self.member_msg_count.keys())[0]
+            for member in self.member_msg_count:
+                if self.member_msg_count[member] > self.member_msg_count[self.touch_grass_member]:
+                    self.touch_grass_member = member
+            # transfer role
+            await self.touch_grass_member.add_roles(self.touch_grass_role)
+
+    # background task
     @tasks.loop(time=daily_reset_time)
     async def reset_daily_counts(self):
         print("Daily message count reset!")
+        # reset counts
         for member in self.daily_msg_count:
             self.daily_msg_count[member] = 0
+        # clear the must go outside member list
+        self.must_go_outside = []
 
-    # helpers
+    # helper
     def init_msg_counts(self):
-        for member in self.bot.get_all_members():
-            if member.bot:
-                continue
+        for member in self.member_msg_count:
             self.daily_msg_count[member] = 0
+            print(member, self.member_msg_count[member])
